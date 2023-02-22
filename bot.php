8631b3c8d291d511e24f81342dd2674b459cc8b9
Ulu-pro <web.ulu7@gmail.com>
2023-02-22 22:01:34 +0500

    Bot library: updated single to double quotes

--- bot.php
+++ bot.php
@@ -1,7 +1,7 @@
 <?php
 class Bot {
-  private $url;
-  private $data;
+  private string $url;
+  private mixed $data;
 
   public function __construct($token) {
     $this->url = "https://api.telegram.org/bot$token/";
@@ -10,23 +10,23 @@ class Bot {
 
   public function getData() {
     if (empty($this->data)) {
-      $raw_data = file_get_contents('php://input');
+      $raw_data = file_get_contents("php://input");
       return json_decode($raw_data, true);
     } else {
       return $this->data;
     }
   }
 
-  public function request($method, $params) {
-    return file_get_contents($this->url . $method . '?' . http_build_query($params));
+  public function request($method, $params): bool|string {
+    return file_get_contents($this->url . $method . "?" . http_build_query($params));
   }
 
   public function sendMessage($chat_id, $text, $reply_markup = []) {
-    $_ = $this->request('sendMessage', [
-        'chat_id' => $chat_id,
-        'text' => $text,
-        'parse_mode' => 'Markdown',
-        'reply_markup' => $reply_markup
+    $_ = $this->request("sendMessage", [
+        "chat_id" => $chat_id,
+        "text" => $text,
+        "parse_mode" => "Markdown",
+        "reply_markup" => $reply_markup
     ]);
     unset($_);
   }
