bda6f2cbf72dba66b669f2ab61c4e47ecec7bbeb
Ulu-pro <web.ulu7@gmail.com>
2023-02-25 09:39:09 +0500

    Initialized main index page for handler of updates from API

--- index.php
+++ index.php
@@ -2,40 +2,33 @@
 require "bot.php";
 require "env.php";
 require "text.php";
+require "types.php";
 
 $bot = new Bot(Env::TOKEN);
 $data = $bot->getData();
+$app = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]/app/";
 file_put_contents("log.txt", json_encode($data, true)."\n", FILE_APPEND);
+
+if (!isset($data["message"])) {
+  exit;
+}
 
 $message = $data["message"];
 $chat_id = $message["chat"]["id"];
 $text = $message["text"];
-
-function keyboard($rows): string {
-  return json_encode([
-      "resize_keyboard" => true,
-      "keyboard" => $rows
-  ], true);
-}
 
 if ($text == "/ping" && in_array($chat_id, Env::ADMIN_ID)) {
   $bot->sendMessage($chat_id, Text::PING);
 }
 
 else if ($text == "/start") {
-  $sign_up = keyboard([
-      [
-          [
-              "text" => Text::SIGN_UP,
-              "request_contact" => true
-          ]
-      ]
-  ]);
-  $bot->sendMessage($chat_id, Text::START, $sign_up);
+  $bot->sendMessage($chat_id, Text::START, keyboard([
+    [
+      button(Text::ORDER, $app)
+    ]
+  ]));
 }
 
-else if (isset($message["contact"])) {
-  $phone = $message["contact"]["phone_number"];
-  $phone = substr($phone, 1);
-  $bot->sendMessage($chat_id, $phone);
+else {
+  $bot->sendMessage($chat_id, Text::UNKNOWN);
 }
