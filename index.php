7ff355de4baf80143ff2b92bfe461f739c02b450
Ulu-pro <web.ulu7@gmail.com>
2023-02-26 17:01:57 +0500

    Handling basic commands and set inline keyboard to message

--- index.php
+++ index.php
@@ -3,32 +3,47 @@ require "bot.php";
 require "env.php";
 require "text.php";
 require "types.php";
+require "db.php";
 
 $bot = new Bot(Env::TOKEN);
 $data = $bot->getData();
 $app = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]/app/";
 file_put_contents("log.txt", json_encode($data, true)."\n", FILE_APPEND);
 
-if (!isset($data["message"])) {
-  exit;
-}
-
 $message = $data["message"];
 $chat_id = $message["chat"]["id"];
 $text = $message["text"];
 
-if ($text == "/ping" && in_array($chat_id, Env::ADMIN_ID)) {
-  $bot->sendMessage($chat_id, Text::PING);
-}
-
-else if ($text == "/start") {
-  $bot->sendMessage($chat_id, Text::START, keyboard([
-    [
-      button(Text::ORDER, $app)
-    ]
+function keyboard_markup($message, $inline, $url) {
+  global $bot, $chat_id;
+  $bot->sendMessage($chat_id, $message, keyboard([
+      [
+          button($inline, $url)
+      ]
   ]));
 }
 
-else {
-  $bot->sendMessage($chat_id, Text::UNKNOWN);
+switch (substr($text, 1)) {
+  case "start":
+    keyboard_markup(
+        Text::START,
+        Text::START_BUTTON,
+        $app
+    );
+    break;
+  case "orders":
+    // TODO: show orders page of chat_id
+    keyboard_markup(
+        "Order",
+        "Button",
+        $app
+    );
+    break;
+  case "ping":
+    if (in_array($chat_id, Env::ADMIN_ID)) {
+      $bot->sendMessage($chat_id, Text::PING);
+    }
+    break;
+  default:
+    $bot->sendMessage($chat_id, Text::HELP);
 }
