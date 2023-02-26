ce554b48bf971a6fb7f9fa730064d2d0e268a4a7
Ulu-pro <web.ulu7@gmail.com>
2023-02-26 17:00:26 +0500

    Show categories table: test

--- app/index.php
+++ app/index.php
@@ -1,3 +1,8 @@
+<?php
+require "../env.php";
+require "../db.php";
+if (!isset($db)) exit;
+?>
 <!DOCTYPE html>
 <html lang="uz">
 <head>
@@ -6,17 +11,20 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Mister Food</title>
-  <style>img[alt="www.000webhost.com"]{display:none}</style>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h1>Hello!!!</h1>
-<div id="theme"></div>
+  <?php
+  $db->select(Tables::CATEGORY, function ($category) {
+    [$id, $name] = $category;
+    echo "$id: $name";
+  });
+  ?>
 <script src="https://telegram.org/js/telegram-web-app.js?0"></script>
 <!--suppress JSUnresolvedVariable-->
 <script>
 let App = window.Telegram.WebApp
 App.MainButton.show()
-document.querySelector("#theme").innerHTML = JSON.stringify(App.themeParams)
 </script>
 </body>
 </html>
