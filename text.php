e74a8fa3d3972ffe179f931c0acdc494d12d17d3
Ulu-pro <web.ulu7@gmail.com>
2023-03-04 21:00:36 +0500

    Web App's text fields

--- text.php
+++ text.php
@@ -6,6 +6,12 @@ Ajoyib tushlikka buyurtma bering!
 ";
   const START_BUTTON = "Buyurtma berish";
   const BUTTON_TOTAL_ORDER = "JAMI BUYURTMA";
+  const BUTTON_PAY_ORDER = "%s UZS";
+  const ORDER_HEADER = "BUYURTMA";
+  const ORDER_EDIT = "O'zgartirish";
+  const ORDER_COMMENT_PLACEHOLDER = "Izoh qoldirish...";
+  const ORDER_COMMENT_DESCRIPTION =
+      "Maxsus so'rovlar, tafsilotlarlar uchun";
   const PING = "Pong!";
   const HELP = "
 ❓ *Buyruqlar*:\n
