a8b9ecdee9a3828877d43d64160fe566678b27be
Ulu-pro <web.ulu7@gmail.com>
2023-03-04 21:07:17 +0500

    Script which ready to send order data to API with chat_id

--- app/script.js
+++ app/script.js
@@ -1,6 +1,11 @@
 // noinspection JSUnresolvedVariable,JSUnresolvedFunction
 
+const menu  = document.querySelector('.menu');
+const order = document.querySelector('.order');
 const cards = document.querySelectorAll('.card');
+const items = document.querySelectorAll('.item');
+const edit = document.querySelector('.edit');
+
 cards.forEach(card => {
   const plus = card.querySelector('.counter-plus');
   const minus = card.querySelector('.counter-minus');
@@ -24,6 +29,9 @@ cards.forEach(card => {
     }
     toggleMainButton();
   });
+  const price = card.querySelector('.price');
+  const type_price = card.getAttribute('data-type-price');
+  price.innerText = formattedPrice(type_price);
 });
 
 function toggleMainButton(display = false) {
@@ -36,13 +44,94 @@ function toggleMainButton(display = false) {
       if (parseInt(quantity.innerText) !== 0) {
         hidden = false;
       }
-    })
+    });
     if (hidden) {
       App.MainButton.hide();
     }
   }
 }
 
+edit.addEventListener('click', switchToMenu);
+App.BackButton.onClick(switchToMenu);
+
+function switchToMenu() {
+  order.style.display = 'none';
+  menu.style.display = 'flex';
+  App.BackButton.hide();
+  App.MainButton.text = total_order_text;
+}
+
 App.MainButton.onClick(() => {
-  window.location.href = 'images/1.jpg'
-})
+  const order_items = getOrderItems();
+  const total_price = orderTotalPrice(order_items);
+  App.MainButton.text = formattedPrice(total_price);
+  if (window.getComputedStyle(menu, null).display === 'flex') {
+    menu.style.display = 'none';
+    order.style.display = 'block';
+    App.BackButton.show();
+    renderSelectedItems(order_items);
+  }
+  else if (window.getComputedStyle(order, null).display === 'block') {
+    // TODO: order is ok (ultimate), process to api
+    alert(JSON.stringify(order_items));
+    const order_data = {
+      chat_id: App.initDataUnsafe.user.id,
+      order_items: order_items
+    };
+    apiOrderData(order_data);
+  }
+});
+
+function getOrderItems() {
+  let order_items = [];
+  cards.forEach(card => {
+    const quantity = card.querySelector('.quantity');
+    const type_quantity = parseInt(quantity.innerText);
+    if (type_quantity !== 0) {
+      const type_id = card.getAttribute('data-type-id');
+      const type_price = card.getAttribute('data-type-price');
+      order_items.push({
+        id: parseInt(type_id),
+        quantity: type_quantity,
+        price: type_quantity * type_price
+      });
+    }
+  });
+  return order_items;
+}
+
+function orderTotalPrice(order_items) {
+  let total_price = 0;
+  order_items.forEach(item => {
+    total_price += item.price;
+  });
+  return total_price;
+}
+
+function renderSelectedItems(order_items) {
+  items.forEach(item => {
+    item.style.display = 'none';
+  })
+  order_items.forEach(item => {
+    const order_item = document.querySelector(`[data-item-id="${item.id}"]`);
+    const quantity = order_item.querySelector('.quantity');
+    const price = order_item.querySelector('.price');
+    quantity.innerText = item.quantity;
+    price.innerText = formattedPrice(item.price);
+    order_item.style.display = 'block';
+  });
+}
+
+function apiOrderData(order_data) {
+  /*fetch('api.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(order_data)
+  })
+      .then(response => response.json())
+      .then(_ => {
+        // alert(data);
+      });*/
+}
