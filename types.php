8b68022754954bade5c395a74c18feb8751a7d6a
Ulu-pro <web.ulu7@gmail.com>
2023-03-06 22:10:06 +0500

    LabeledPrice + pay option for Inline Button

--- types.php
+++ types.php
@@ -1,19 +1,27 @@
-<?php
+<?php /** @noinspection PhpArrayShapeAttributeCanBeAddedInspection */
 function keyboard($rows): string {
   return json_encode([
       "inline_keyboard" => $rows
-  ], true);
+  ], JSON_HEX_TAG);
 }
 
-/** @noinspection PhpArrayShapeAttributeCanBeAddedInspection */
-function button($text, $web_app = ""): array {
+function button($text, $web_app = null, $pay = false): array {
   $array = [
       "text" => $text
   ];
-  if ($web_app != "") {
+  if ($web_app != null) {
     $array["web_app"] = [
         "url" => $web_app
     ];
+  } else if ($pay) {
+    $array["pay"] = true;
   }
   return $array;
+}
+
+function labeledPrice($label, $amount): array {
+  return [
+      "label" => $label,
+      "amount" => $amount
+  ];
 }
