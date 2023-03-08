2e93f2bbd5bc45e47abd7f484b797d07daefc022
Ulu-pro <web.ulu7@gmail.com>
2023-03-08 19:58:34 +0500

    Modified button function and added some others

--- types.php
+++ types.php
@@ -5,7 +5,8 @@ function keyboard($rows): string {
   ], JSON_HEX_TAG);
 }
 
-function button($text, $web_app = null, $pay = false): array {
+function button($text, $web_app = null, $pay = false,
+                $callback_data = ""): array {
   $array = [
       "text" => $text
   ];
@@ -15,6 +16,8 @@ function button($text, $web_app = null, $pay = false): array {
     ];
   } else if ($pay) {
     $array["pay"] = true;
+  } else if ($callback_data != "") {
+    $array["callback_data"] = $callback_data;
   }
   return $array;
 }
@@ -23,5 +26,19 @@ function labeledPrice($label, $amount): array {
   return [
       "label" => $label,
       "amount" => $amount
+  ];
+}
+
+function price_format($amount, $decimal = null): string {
+  return sprintf(Text::BUTTON_PAY_ORDER,
+      number_format($amount, $decimal ?? log10(Env::DECIMAL),
+          ",", " ")
+  );
+}
+
+function more_details_row($app, $order_id, $chat_id): array {
+  return [
+      button(Text::MORE_DETAILS, $app .
+          "view.php?order_id=$order_id&chat_id=$chat_id")
   ];
 }
